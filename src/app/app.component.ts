16fac470064c7c44b7fcfd05473ade66c73fe30a
mikeangeloo <mikeangeloo87@outlook.com>
2021-12-02 19:25:09 -0500

    mod menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,21 @@ import {MenuController, Platform} from "@ionic/angular";
 import {BehaviorSubject, filter, Subject, takeUntil} from "rxjs";
 import {SessionService} from "./services/session.service";
 import {ProfileDataI} from "./interfaces/profile/profile-data.interface";
-
+export interface MenuTreeI
+{
+  module: string;
+  level: number;
+  icon: string;
+  label: string;
+  route: string;
+  children?: MenuTreeI [];
+}
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent implements OnDestroy {
   public destroyed = new Subject<any>();
   public showMenu = true;
@@ -18,8 +27,49 @@ export class AppComponent implements OnDestroy {
   public subMenuOpen = false;
   public _navRoute: string;
 
-
-
+  public demoMenu: MenuTreeI[] = [
+    {
+      module: 'people',
+      icon: 'people',
+      label: 'Primer Nivel',
+      route: '/test',
+      level: 0,
+      children: [
+        {
+          module: 'people',
+          icon: 'people',
+          label: 'Segundo Nivel',
+          route: '/test/test2',
+          level: 1,
+          children: [
+            {
+              module: 'people',
+              icon: 'people',
+              label: 'Tercer 3.1 Nivel',
+              route: '/test/test2/test3',
+              level: 3,
+              children: [
+                {
+                  module: 'people',
+                  icon: 'people',
+                  label: 'Cuarto Nivel',
+                  route: '/test/test2/test3/test/4',
+                  level: 4,
+                }
+              ]
+            },
+            {
+              module: 'people',
+              icon: 'people',
+              label: 'Tercer 3.2 Nivel',
+              route: '/test/test2/test3',
+              level: 3,
+            }
+          ]
+        }
+      ]
+    }
+  ];
   constructor(
     public route: ActivatedRoute,
     private platform: Platform,
